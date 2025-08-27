3b666facbce661746fc0f06fdf07e84749b332b3
Romeo-04 <147164919+Romeo-04@users.noreply.github.com>
2025-08-27 09:58:31 +0800

    stop tracking venv and added .gitignore

--- Expenses_Calculator/expenses/admin.py
+++ Expenses_Calculator/expenses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Expense
 
-# Register your models here.
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'amount', 'timestamp', 'description', 'category')
+    search_fields = ('name', 'category')
+    list_filter = ('timestamp', 'category')
+    ordering = ('-timestamp',)
+    
+
--- Expenses_Calculator/expenses/models.py
+++ Expenses_Calculator/expenses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    """Expense Model fields"""
+    CATEGORY_CHOICES = (
+        ('Food', 'Food'),
+        ('Transport', 'Transport'),
+        ('Entertainment', 'Entertainment'),
+    )
+
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=32, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return f'{self.name} - {self.amount}'
+
